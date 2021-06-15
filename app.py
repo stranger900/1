9455b731d76cd43e895be337d6399ec93ae7cb12
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-06-15 11:26:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return ('Hello world!')
 
-for variable, value in os.envoron.items():
-    app.config[settings.env] = value
-
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
     
