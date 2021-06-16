1cad5f05d03cd8f9c85fe821fdd1c5792e9800ec
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-06-16 10:37:42 -0400

    add template for app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 
-from flask import Flask
+import sys
+import os
+from flask import Flask, render_template, redirect, url_for, request
 
+#, template_folder= '/home/ubuntu/1'
 app = Flask(__name__)
 
+#@app.route('/')
+#def hello_world():
+#    return ('Hello world!')
+
 @app.route('/')
-def hello_world():
-    return ('Hello world!')
+def get_incorrect_answers():
+    print ('Hello world!')
+    with open("settings.env", "r") as file:
+        info = file.readlines()
+    
+    return render_template('index.html', info=info)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
+
+
