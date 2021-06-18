48e975dd17d1cf3d213261281ee126edd319b75a
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-06-18 05:58:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 
+#import sys
 #import os
+#from flask import Flask, render_template, redirect, url_for, request
+
 from flask import Flask, render_template
 
+#, template_folder= '/home/ubuntu/1'
 app = Flask(__name__)
 
-#variables = [os.environ['TEST_ENV'], 
-#             os.environ['LOGIN']],
-#             os.environ['IMAGE_NAME'],
-#             os.environ['BRANCH_NAME'],
-#             os.environ['BUILD_NAMBER']]
-
-#with open ("settings.env", "w") as file:
-#     for line in variables:
-#        file.write(line + "\n")
+#@app.route('/')
+#def hello_world():
+#    return ('Hello world!')
 
 @app.route('/')
-def get_var():
-    return ('Hello world!')
-#    with open("settings.env", "r") as file:
-#        info = file.readlines()
+def get_incorrect_answers():
+    print ('Hello world!')
+    with open("settings.env", "r") as file:
+        info = file.readlines()
 
-#    return render_template('index.html', info=info)
+    return render_template('index.html', info=info)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
