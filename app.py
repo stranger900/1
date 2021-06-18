5fafec5f1f8e6e48f06faabb484294fb978bc07b
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-06-18 13:14:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+variables = [ os.environ['TEST_ENV'], 
+              os.environ['LOGIN'],
+              os.environ['IMAGE_NAME'],
+              os.environ['BRANCH_NAME'],
+              os.environ['BUILD_NUMBER'] ]
 
+
+with open ("settings.env", "w") as file:
+     for line in variables:
+        file.write(line + "\n")
 
 @app.route('/')
 def get_variables():
