1dc039d48121e7932a8a3967bc3e34bb7001a182
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-06-18 05:01:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ from flask import Flask, render_template
 #, template_folder= '/home/ubuntu/1'
 app = Flask(__name__)
 
+variables = [os.environ['TEST_ENV'], 
+             os.environ['token']]
 
+with open ("settings.env", "w") as file:
+     for line in variables:
+        file.write(line + "\n")
 
 @app.route('/')
 def get_incorrect_answers():
