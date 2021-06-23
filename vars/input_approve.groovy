4eceebfaa049374ca9ec0b7c217d84bac560ccc1
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-06-23 12:22:39 +0300

    Update input_approve.groovy

--- vars/input_approve.groovy
+++ vars/input_approve.groovy
@@ -2,8 +2,9 @@ def call(){
      input(
                  id: 'userInput',
                  message: 'Should we continue?', 
-                 ok: 'Approve'
+                 ok: 'Approve',
                  parameters: [       string(defaultValue: 'Click "Aprove" to PROCEED',
                                             description: 'Do you want to approve',
-                                            name: 'Approval'), ])       
+                                            name: 'Approval'), 
+                             ])       
 }
