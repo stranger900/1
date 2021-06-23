54d94594a5bcd2e8a0e2f1c547e996f2926ae417
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-06-23 11:32:15 +0300

    Update input_approve.groovy

--- vars/input_approve.groovy
+++ vars/input_approve.groovy
@@ -1,9 +1,7 @@
-def input_approve(){
-     input {
-                message "Should we continue?"
-                ok "Yes, we should."                
-                parameters {
-                    string(name: 'Approval', defaultValue: 'YES', description: 'Are you sure to continue ?')
-                }
-            }
+def input_approve(){     
+     input(
+                 message: 'Should we continue?', 
+                 parameters: 
+                 [name: 'Approval', defaultValue: 'YES', description: 'Are you sure to continue ?']
+          )       
 }
