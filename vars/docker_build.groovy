9753385e191929a31629c9e7bc734cb8ae03d04a
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-06-23 13:25:47 +0300

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
 def call(Map config = [:]){
-    sh 'docker build -t ${config.DOCKERHUB_CRED_USR}/${config.IMAGE_NAME}:${config.BRANCH_NAME}-${config.BUILD_NUMBER} .'     
+    sh 'docker build -t ${config.dockerhub_cred}/${config.image_name}:${config.branch_name}-${config.build_number} .'     
 }
