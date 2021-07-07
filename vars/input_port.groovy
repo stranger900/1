954cc1d46a9c612e6436a6cb5abe10827cd3b014
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-07-07 08:53:09 +0300

    Update input_port.groovy

--- vars/input_port.groovy
+++ vars/input_port.groovy
@@ -1,7 +1,7 @@
 def call(){     
      env.PORT_NUMBER = input(
         id: 'userInput', message: 'Let\'s promote?', parameters: [
-        [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env']
+        [$class: 'TextParameterDefinition', defaultValue: 'Enter port number for docker container ( 5010 - 6000 )', description: 'Port number', name: 'pn']
         ])
      echo ("Env: "+env.PORT_NUMBER)
 }
