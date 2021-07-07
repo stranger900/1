80b67240a2fb10c5f30530fb39bfe5ecff23c6da
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-07-07 07:34:18 +0300

    Update input_port.groovy

--- vars/input_port.groovy
+++ vars/input_port.groovy
@@ -1,7 +1,7 @@
 def call(){     
-     PORT_NUMBER = input(
+     env.PORT_NUMBER = input(
         id: 'userInput', message: 'Let\'s promote?', parameters: [
         [$class: 'TextParameterDefinition', defaultValue: 'uat', description: 'Environment', name: 'env']
         ])
-     echo ("Env: "+PORT_NUMBER)
+     echo ("Env: "+env.PORT_NUMBER)
 }
