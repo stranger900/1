d27c6b60c7f523b5e81af0d76a8d51c762c2ffdf
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-07-27 07:33:04 -0400

    Update test_app.py

--- docker/test_app.py
+++ docker/test_app.py
@@ -1,19 +1,13 @@
-import unittest, requests, os
-#from app import app, variables
-
+import unittest, requests, os, sys
+#from sys import argv
 class PageLoadTest(unittest.TestCase):
+
     def test_stat(self):
+        print(sys.argv[0])
+        print(str(sys.argv[1]))
         response = requests.get('http://192.168.1.15:5010')
         assert response.status_code == 200
 
-#     def test_status(self):
-#         tester = app.test_client(self)
-#         response = tester.get("/fo")
-#         statuscode = response.status_code
-#         self.assertEqual(statuscode, 404)
-
-#     def test_var(self):
-#         self.assertEqual(os.environ['LOGIN'], 'andriy')
-
 if __name__ == '__main__':
-    unittest.main()
+     unittest.main()
+
