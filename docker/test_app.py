a4eec3188b55f5a9cfd5d1dc44315700c4c21312
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-07-27 10:53:51 -0400

    Update test_app.py

--- docker/test_app.py
+++ docker/test_app.py
@@ -1,13 +1,30 @@
+
 import unittest, requests, os, sys
-#from sys import argv
-class PageLoadTest(unittest.TestCase):
 
-    def test_stat(self):
-        print(sys.argv[0])
-        print(str(sys.argv[1]))
-        response = requests.get('http://192.168.1.15:5010')
-        assert response.status_code == 200
+ip =  sys.argv[1]
+port = sys.argv[2]
 
-if __name__ == '__main__':
-     unittest.main()
+print("Testing :  " + str(sys.argv[1]) + ":" + str(sys.argv[2]))
 
+response = requests.get("http://" + ip + ":" + port)
+ping = os.system("ping -c 1 " + ip)
+
+if (response.status_code == 200):
+     print("************************")
+     print("  Server responded with status 200")
+     print("  Test OK  ")
+     print("************************")
+else:
+     print("ERROR ***** Something wrong") 
+
+if   (ping == 0):
+     print("************************")
+     print("  Test connection OK ")
+     print("************************")
+else:
+     print("ERROR ***** Something wrong") 
+
+
+
+#if __name__ == '__main__':
+#     unittest.main()
