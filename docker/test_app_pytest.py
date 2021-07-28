80733add5b7726c9f6e31297308c409e5625faa5
stranger900 <36480357+stranger900@users.noreply.github.com>
2021-07-28 16:01:19 -0400

    Create test_app_pytest.py

---
+++ docker/test_app_pytest.py
@@ -0,0 +1,9 @@
+import requests, sys, os
+
+def test_status():
+    r = requests.get('http://' + sys.argv[2] + ':' + sys.argv[3])
+    assert r.status_code == 200
+
+def test_connection():
+    ping = os.system("ping -c 1" + sys.argv[2])
+    assert ping == 0
